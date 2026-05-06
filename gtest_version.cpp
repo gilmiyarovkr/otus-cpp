f393bf6214dbf829b05e7ad5f519b9758944943d
gilmiyarovkr <krgilmiyarov@gmail.com>
2026-05-06 21:56:10 +0300

    Добавлен тест версии googletest. В автоматизации включена обработки googletest.

---
+++ gtest_version.cpp
@@ -0,0 +1,8 @@
+#include "lib.h"
+
+#include <gtest/gtest.h>
+
+TEST(VersionTest, CheckPatchVersion) {
+    // Пример проверки, замените на вашу реальную логику
+    EXPECT_GT(PROJECT_VERSION_PATCH, 0); 
+}
